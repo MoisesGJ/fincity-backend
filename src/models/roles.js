9d424cd51d313034ac4ae4f41325580540fc5234
DEVFBA <angenrique.gutierrez@gmail.com>
2024-06-21 20:27:27 -0600

    Feat: Role Model

---
+++ src/models/roles.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const roleSchema = new Schema(
+  {
+    description: {
+      type: String,
+      maxLength: 20,
+      required: true,
+      unique: true
+    },
+    page: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Role = mongoose.model('roles', roleSchema)
+
+model.exports = Role
