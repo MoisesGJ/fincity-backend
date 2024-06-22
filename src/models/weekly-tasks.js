afb3287b19f8cc06a0c873114b02ed4378763ccf
DEVFBA <angenrique.gutierrez@gmail.com>
2024-06-21 21:14:59 -0600

    Feat: Weekly Task Model

---
+++ src/models/weekly-tasks.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const weeklyTaskSchema = new Schema(
+  {
+    group: {
+      type: Schema.Types.ObjectId,
+      ref: 'groups'
+    },
+    start_date: {
+      type: Date,
+      required: [true, 'Start Date is required']
+    },
+    final_date: {
+      type: Date,
+      required: [true, 'Final Date is required']
+    },
+    task: {
+      type: Schema.Types.ObjectId,
+      ref: 'tasks'
+    },
+    times_per_week: {
+      type: Number
+    },
+    value: {
+      type: Number
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Weekly_Task = mongoose.model('weekly_tasks', weeklyTaskSchema)
+
+model.exports = Weekly_Task
